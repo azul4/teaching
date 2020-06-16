1ac3ddbb5fc6bba3292d999286ceeae77ac0e742
azul4 <66548646+azul4@users.noreply.github.com>
2020-06-16 22:04:36 +0900

    Create 11655 ROT13.py

---
+++ 11655 ROT13.py
@@ -0,0 +1,11 @@
+#미완성
+
+alphabet_big = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+alphabet_small = list('abcdefghijklmnopqrstuvwxyz')
+
+n = list(input())
+to_print = []
+for i in n:
+    if i.islower():
+    else:
+        
