e75e41a2fa39a26b3d771a82d361db2c06ed3c1f
azul4 <66548646+azul4@users.noreply.github.com>
2020-06-27 02:45:04 +0900

    Update 11655 ROT13.py

--- 11655 ROT13.py
+++ 11655 ROT13.py
@@ -1,11 +1,23 @@
-#미완성
+#
 
-alphabet_big = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-alphabet_small = list('abcdefghijklmnopqrstuvwxyz')
+def print_list(li):
+    for i in li:
+        print(i, end='')
 
-n = list(input())
-to_print = []
-for i in n:
-    if i.islower():
-    else:
         
+alphabet_big = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alphabet_small = 'abcdefghijklmnopqrstuvwxyz'
+big = 65
+small = 97
+n = list(input())
+
+for i in range(len(n)):
+    if n[i] in alphabet_small:
+        n[i] = alphabet_small[(k+12)%len(n)]
+    elif n[i] in alphabet_big:
+        k = alphabet_big.index(n[i])
+        n[i] = alphabet_big[(k+12)%len(n)]
+    else:
+        pass
+
+print_list(n)
